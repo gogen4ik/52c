0f32e2737443967d28b42f4e92ac1e1557d842b9
gogen4ik <104084678+gogen4ik@users.noreply.github.com>
2023-12-26 16:18:17 +0300

    Update script.js

--- Primer1/script.js
+++ Primer1/script.js
@@ -4,3 +4,18 @@ const sum = a + b;
 function showMessage() {
     alert(sum);
   }
+
+// Плавный скролл при клике на ссылки меню
+var menuLinks = document.querySelectorAll('.menu a');
+    for(var i = 0; i < menuLinks.length; i++) {
+        menuLinks[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            var targetSectionId = this.getAttribute('href').substr(1);
+            var targetSection = document.getElementById(targetSectionId);
+            var targetSectionOffsetTop = targetSection.offsetTop;
+            window.scrollTo({
+                top: targetSectionOffsetTop,
+                behavior: 'smooth'
+            });
+        });
+    }
